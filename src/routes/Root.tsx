03fa63632c40a7a4d0281e623a10bdca4474be0d
Facundo Rocha <facundo.rocha@outlook.com>
2022-12-30 22:58:33 -0300

    Added light and dark theme system

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -1,11 +1,16 @@
 import { Outlet } from 'react-router-dom'
-import App from 'App'
+import { App } from 'App'
+import { ThemeProvider } from 'contexts/ThemeContext'
+import { Header } from 'components'
 
 export const Root = () => {
   return (
     <div className='App'>
-      <App />
-      <Outlet />
+      <ThemeProvider>
+        <Header />
+        <App />
+        <Outlet />
+      </ThemeProvider>
     </div>
   )
 }
